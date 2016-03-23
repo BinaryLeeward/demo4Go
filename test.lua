e172e82dfcc6c9b5868f06c480ff18b63d67363c
unknown <binaryleeward@gmail.com>
2016-03-23 14:48:10 +0800

    touchMove for add next list user

--- test.lua
+++ test.lua
@@ -1,10 +1,17 @@
-head = 145;
-perLine = 128;
+head = 150;
+perLine = 130;
 hiBtnHeight = 95;
 hiBtnWidth = 670;
 addBtnHeight = 64;
 addBtnWidth = 144;
+touchMoveCount = 2;
+touchSleepMills = 100;
+waitMills = 1000;
 init(0);
+
+
+local sz = require("sz")
+local pos = sz.pos
 
 function findBtnAndTouch(color,btnWidth,btnHeight)
 	x, y = findColorInRegionFuzzy(color, 100, 0, head,  width, height); 
@@ -32,34 +39,52 @@ function findBtnAndTouch(color,btnWidth,btnHeight)
 	end
 
 	touchDown(hiBtnPointX, hiBtnPointY);
-	mSleep(30);
+	mSleep(touchSleepMills);
     touchUp(hiBtnPointX,hiBtnPointY);
     return hiBtnPointX, hiBtnPointY;
 end
 
+toast("正在打开微信附近的人"); 
 r = runApp("com.tencent.mm","com.tencent.mm.plugin.nearby.ui.NearbyFriendsUI");
 if r==0 then
 	width,height = getScreenSize(); 
-	length = (height-head)/perLine
-    mSleep(3000);
-    for i=1,length,1 do
-    	mSleep(1000);
-    	touchDown(width/2, (head+(length/2))*i);
-    	mSleep(30);
-   		touchUp(width/2, (head+(length/2))*i);
-	    mSleep(3000);
-		x,y = findBtnAndTouch(0x45c01a,hiBtnWidth,hiBtnHeight)
-	    if x>-1 and y>-1 then
-	    	mSleep(3000);
-	    	x1,y2 = findBtnAndTouch(0x45c01a,addBtnWidth,addBtnHeight)
-	    	os.execute("input keyevent 4");
-	    	mSleep(1000);
+	length = math.floor((height-head)/perLine);
+    mSleep(5*waitMills);
+    toast("开始打招呼"); 
+    for i=1,touchMoveCount,1 do
+    	touchX = width/2;
+	    for i=0,length-1,1 do
+	    	mSleep(waitMills);
+	        touchY = (head+perLine/2)+ (perLine*i);
+	    	touchDown(touchX, touchY);
+	    	mSleep(touchSleepMills);
+	   		touchUp(touchX, touchY);
+		    mSleep(waitMills);
+			x,y = findBtnAndTouch(0x45c01a,hiBtnWidth,hiBtnHeight)
+		    if x>-1 and y>-1 then
+		    	mSleep(waitMills);
+		    	x1,y2 = findBtnAndTouch(0x45c01a,addBtnWidth,addBtnHeight)
+		    	os.execute("input keyevent 4");
+		    	mSleep(waitMills);
+		    	os.execute("input keyevent 4");
+		    end
+		    mSleep(waitMills);
 	    	os.execute("input keyevent 4");
 	    end
-	    mSleep(2000);
-    	os.execute("input keyevent 4");
-    end
-	
+	    mSleep(waitMills);
+
+		local p0 = pos(touchX, (head+ perLine*length))
+		local p1 = pos(touchX,head)
+	    p0:touchMoveTo(p1,5,touchSleepMills,10);
+	    --[[ouchDown(touchX, (head+ perLine*length));
+	    mSleep(touchSleepMills);
+	    touchMove(touchX,head);
+	    mSleep(touchSleepMills);
+	    touchUp(touchX,head);
+	    ]]
+	    mSleep(waitMills);
+	end
+	toast("打招呼结束"); 
     dialog(x.."  "..y);
 else
 	dialog("启动失败");
